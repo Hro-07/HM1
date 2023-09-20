12ce5daf18ec62d91ca03370e8625009cb57271d
Hro-07 <hrostepanyan5@gmail.com>
2023-09-20 12:08:17 +0200

    Initial Commit

---
+++ HM1/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  HM1
+//
+//  Created by Hrayr on 19/9/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
